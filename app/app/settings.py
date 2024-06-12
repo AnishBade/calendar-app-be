eb6f816c1e4c18049a8f020c3bcccd5ea5f5d6d6
Anish Bade <badeanish6045@gmail.com>
2024-06-13 01:19:46 +0545

    add cors allowed origin

--- app/app/settings.py
+++ app/app/settings.py
@@ -148,5 +148,5 @@ CELERY_RESULT_BACKEND = "rpc://"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://anish-bade-calendar-app.netlify.app/",
+    "https://anish-bade-calendar-app.netlify.app",
 ]
